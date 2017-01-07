9e113d334881ad636422f385a33386136636356e
Zitong He <me@hezt.net>
2017-01-07 15:00:18 +0800

    merge module Main and module Padding. Done.

--- Main.sv
+++ Main.sv
@@ -24,68 +24,88 @@
 
 module Main(
 	input wire clk,
+	input wire [0:1] enable,
 	input wire [0:7] oneByte,
-	input wire enable,
-	input wire [0:63] length,
+	output reg [0:511] messageBlock,
 	output reg [0:255] outputV
     );
-
-reg [0:7] holdOneByte;
-reg [0:511] messageBlock, holdMessageBlock;
-reg [0:31] extendedWord [0:67];
-reg [0:31] extendedWordPrime [0:63];
-reg [0:9] messageBlockLengthConuter = 0;
+reg [0:63] counter = 0;
+reg [0:63] length = 0;
 reg [0:255] inputV;
-reg started = 0;
-reg padEnable = 0;
 reg cfEnable = 0;
-reg padFinished, cfFinished;
 
-
-Padding padding(
+reg [0:511] holdMessageBlock;
+reg lastBlockStart = 0;
+CompressFunction compressfunction(
 	.clk         (clk),
-	.enable      (padEnable),
-	.oneByte     (holdOneByte),
-	.length      (length),
-	.messageBlock(messageBlock),
-	.finished   (padFinished)
+	.enable      (cfEnable),
+	.inputV      (inputV),
+	.messageBlock(holdMessageBlock),
+	.outputV     (outputV),
+	.finished    (cfFinished)
 	);
+always_ff @(posedge clk) begin
+	if(cfEnable == 0) begin
+		if(enable == 0) begin
+			// clear
+			lastBlockStart <= 0;
+			counter <= 0;
+			length <= 0;
+			inputV <= 256'h7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e;
+		end
+		if(enable == 1) begin
+			// copy oneByte to messageBlock
+			messageBlock[counter + 0] <= oneByte[0];
+			messageBlock[counter + 1] <= oneByte[1];
+			messageBlock[counter + 2] <= oneByte[2];
+			messageBlock[counter + 3] <= oneByte[3];
+			messageBlock[counter + 4] <= oneByte[4];
+			messageBlock[counter + 5] <= oneByte[5];
+			messageBlock[counter + 6] <= oneByte[6];
+			messageBlock[counter + 7] <= oneByte[7];
+			counter <= counter + 8;
+			length <= length + 8;
+			if(counter == 512) begin
+				cfEnable <= 1;
+				holdMessageBlock <= messageBlock;
+				counter <= 0;
+			end
+		end
+		if(enable == 2) begin
+			if(counter == 512) begin
+				cfEnable <= 1;
+				holdMessageBlock <= messageBlock;
+				if(lastBlockStart == 0) begin
+					counter <= 0;
+				end
+				if(lastBlockStart == 1) begin
+					counter <= -1;
+				end
+			end
+			// padding with 100...000
+			if(counter < 448 && counter >= 0) begin
+				messageBlock[counter + 0] <= 0;
+				messageBlock[counter + 1] <= 0;
+				messageBlock[counter + 2] <= 0;
+				messageBlock[counter + 3] <= 0;
+				messageBlock[counter + 4] <= 0;
+				messageBlock[counter + 5] <= 0;
+				messageBlock[counter + 6] <= 0;
+				messageBlock[counter + 7] <= 0;
+				counter <= counter + 8;
+			end
+			if(counter == 448) begin
+				messageBlock[448:511] <= length;
+				messageBlock[length % 512] <= 1;
+				counter <= 512;
+				lastBlockStart <= 1;
+			end
+		end
 
-
-CompressFunction compressFunction(
-	.clk              (clk),
-	.enable           (cfEnable),
-	.inputV           (inputV),
-	.messageBlock     (holdMessageBlock),
-	.outputV          (outputV),
-	.finished         (cfFinished)
-	);
-// reg padLock = 0;
-// reg cfLock = 0;
-
-assign blockNum = (length / 512) + 1;
-
-always_ff @(posedge clk or posedge enable) begin
-	if(enable == 1 && started == 0) begin
-		started <= 1;
-		inputV <= 256'h7380166f4914b2b9172442d7da8a0600a96f30bc163138aae38dee4db0fb0e4e; 
 	end
-	holdOneByte <= oneByte;
-
-	// if()
-
-	if(started == 1 && padEnable == 0) begin
-		padEnable <= 1;
-		// padLock <= 1;
-	end
-	if(padFinished == 1 && cfEnable == 0) begin
-		holdMessageBlock <= messageBlock;
-		padEnable <= 0;
-		cfEnable <= 1;
-	end
-	if(cfFinished) begin
+	if(cfFinished == 1) begin
+		cfEnable <= 0;		
 		inputV <= outputV;
-		cfEnable <= 0;
 	end
 end
 endmodule
